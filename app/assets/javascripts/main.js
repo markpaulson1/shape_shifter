ab9fed0b9b51bf5689bf94d339da84aff3a100a6
Mark Paulson <markpaulson1@hotmail.com>
2015-07-18 16:41:32 -0400

    sat updates

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -12,3 +12,26 @@ function moveToNextExercise(nextId){
 		window.open('/exercises/'+nextId)
 	}, 1000);
 }
+
+
+
+// Launch screen log-in form
+$(window).on('load', function(){
+ $('.logInButton').on('click', function(event){
+   event.preventDefault();
+   $('.logInForm').show();
+   $('.signUpButton').hide();
+   $('.logInButton').hide();
+ })  
+})
+
+
+// Launch screen sign-in form
+$(window).on('load', function(){
+ $('.signUpButton').on('click', function(event){
+   event.preventDefault();
+   $('.submitForm').show();
+   $('.logInButton').hide();
+   $('.signUpButton').hide();
+ })  
+})
